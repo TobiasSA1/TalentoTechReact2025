f615c641e0359a65c4c65813f96715bd350dd766
TobiasSA1 <tobiassuarez43@gmail.com>
2025-10-24 23:30:51 -0300

    mas cambios

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from "vite";
+﻿import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+
+// Vercel expone VERCEL="1" en build
+const isVercel = process.env.VERCEL === "1";
+// Cambiá esto si tu repo de GH Pages tuviera otro nombre
+const ghBase = "/TalentoTechReact2025/";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/TalentoTechReact2025/" // nombre EXACTO del repo
+  base: isVercel ? "/" : ghBase
 });

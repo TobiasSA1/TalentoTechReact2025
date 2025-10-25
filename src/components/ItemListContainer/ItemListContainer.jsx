6f22e757bd961eaf61e21f4384cddf337c0030d7
TobiasSA1 <tobiassuarez43@gmail.com>
2025-10-24 23:26:09 -0300

    fix: corrige import de ItemList (ruta relativa correcta)

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import ItemList from "./ItemList";
+import ItemList from "../ItemList/ItemList.jsx";
 import "./ItemListContainer.css";
 
 export default function ItemListContainer() {
